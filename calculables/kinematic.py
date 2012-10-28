2a915bef1c3d6585beda615afe98aa17f079cbec
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2012-10-28 21:32:53 +0100

    add P4 and singleP4 calculables
    Details:
    before introducing these two kinematic variables, I had to get linkdef
    and extendVectorUtil from susycaf, adapting them to the pxpypzE that
    are available in the protos ntuples

---
+++ calculables/kinematic.py
@@ -0,0 +1,38 @@
+import supy
+import ROOT as r
+
+lvClass = None
+def LorentzV(*args) :
+    global lvClass
+    if lvClass is None : lvClass = r.Math.LorentzVector(r.Math.PxPyPzE4D('float'))
+    return lvClass(*args)
+
+class singleP4(supy.wrappedChain.calculable) :
+    """
+    Calculable to build Lorentz vector for a single particle.
+    """
+    def __init__(self, collection = None,):
+        self.fixes = collection
+        self.stash(['px', 'py', 'pz', 'ene'])
+    @property
+    def name(self) : return '%s%sP4' % self.fixes
+    def update(self, _) :
+        self.value = LorentzV(self.source[self.px],
+                              self.source[self.py],
+                              self.source[self.pz],
+                              self.source[self.ene])
+
+class P4(supy.wrappedChain.calculable) :
+    """
+    Calculable to build Lorentz vectors for a collection of particles.
+    """
+    def __init__(self, collection = None,):
+        self.fixes = collection
+        self.stash(['px', 'py', 'pz', 'ene'])
+    @property
+    def name(self) : return '%s%sP4' % self.fixes
+    def update(self, _) :
+        self.value = [LorentzV(px, py, pz, e) for px,py,pz,e in zip(self.source[self.px],
+                                                                    self.source[self.py],
+                                                                    self.source[self.pz],
+                                                                    self.source[self.ene])]
--- configuration.py
+++ configuration.py
@@ -5,3 +5,14 @@ def mainTree() :
 
 def leavesToBlackList() :
     return []
+
+def cppROOTDictionariesToGenerate() :
+    return [
+        #("vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > >", "vector;Math/LorentzVector.h"),
+        #ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > etc. is addressed in linkdef.cxx
+        ]
+
+def cppFiles() :
+    return [
+            "cpp/linkdef.cxx",
+            ]
